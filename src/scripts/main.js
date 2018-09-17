2249f56a7140b15af41a32920481dfabe8228f85
Steve Brownlee <chortlehoort@gmail.com>
2018-09-16 23:17:52 -0500

    Integration with Open Food

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,35 @@
-const $ = require("jquery")
+const foodFactory = food => `
+    <section class="foodItem bordered">
+        <header>
+            <h1>${food.name}</h1>
+        </header>
+        <div>
+            ${food.ethnicity} ${food.type}
+        </div>
+        <div>
+            ${food.ingredients.map(f=>f.text).join(", ")}
+        </div>
+    </section>
+`
+const listEl = document.querySelector(".foodlist")
+const addFoodToDom = foodRepresentation => listEl.innerHTML += foodRepresentation
 
-$.ajax("http://localhost:8088/food")
-.then(response => {
-    console.log(response)
-})
-.then(() => {
-    $.ajax({
-        url: "http://localhost:8088/food",
-        method: "POST",
-        data: {
-            name: "Hamburger",
-            type: "meat",
-            ethnicity: "Murica",
-            sideDishes: ["fries", "tater tots", "tater salad"]
-        }
+
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(food => {
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    food.ingredients = productInfo.product.ingredients
+                    food.countryOfOrigin = productInfo.product.countries_tags
+                    food.calories = productInfo.product.nutriments.energy_serving
+                    food.fatPerServing = productInfo.product.nutriments.fat_serving
+                    food.sugarPerServing = productInfo.product.nutriments.sugars_serving
+                    const foodAsHTML = foodFactory(food)
+                    addFoodToDom(foodAsHTML)
+                })
+        })
     })
-})
+
